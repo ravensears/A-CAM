fb2fa9a780b94f092cb5a1d5d146417499dc23d7
Raven Reynolds Sears <ravenrosereynolds@gmail.com>
2022-04-07 21:14:54 +0100

    fixed mongodb, server, and added readme file

--- server/routes/record.js
+++ server/routes/record.js
@@ -50,27 +50,27 @@ recordRoutes.route("/record/add").post(function (req, response) {
   });
 });
 
-// This section will help you update a record by id.
-recordRoutes.route("/update/:id").post(function (req, response) {
-  let db_connect = dbo.getDb();  
-  let myquery = { _id: ObjectId( req.params.id )};  
-  let newvalues = {    
-    $set: {      
-      name: req.body.name,     
-      position: req.body.position,      
-      level: req.body.level,    
-  },  
-};
+// // This section will help you update a record by id.
+// recordRoutes.route("/update/:id").post(function (req, response) {
+//   let db_connect = dbo.getDb();  
+//   let myquery = { _id: ObjectId( req.params.id )};  
+//   let newvalues = {    
+//     $set: {      
+//       name: req.body.name,     
+//       position: req.body.position,      
+//       level: req.body.level,    
+//   },  
+// };
 
-// This section will help you delete a record
-recordRoutes.route("/:id").delete((req, response) => {
-  let db_connect = dbo.getDb();
-  let myquery = { _id: ObjectId( req.params.id )};
-  db_connect.collection("records").deleteOne(myquery, function (err, obj) {
-    if (err) throw err;
-    console.log("1 document deleted");
-    response.json(obj);
-  });
-});
+// // This section will help you delete a record
+// recordRoutes.route("/:id").delete((req, response) => {
+//   let db_connect = dbo.getDb();
+//   let myquery = { _id: ObjectId( req.params.id )};
+//   db_connect.collection("records").deleteOne(myquery, function (err, obj) {
+//     if (err) throw err;
+//     console.log("1 document deleted");
+//     response.json(obj);
+//   });
+// });
 
 module.exports = recordRoutes;
